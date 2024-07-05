6efa647ffb45d6945b8ec6635cc0e84033ecad7a
EnochKingsley83 <141494962+EnochKingsley83@users.noreply.github.com>
2024-07-05 14:18:05 +0800

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -119,6 +119,7 @@ case $choice in
     12)
         # 安装warp到40000端口
         echo "安装warp到40000端口..."
+	apt install gnupg -y
         curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
         echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
         apt update
