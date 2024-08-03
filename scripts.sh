98d9d959e50c4c8e65ac8e54b06ae30035fc1167
EnochKingsley83 <141494962+EnochKingsley83@users.noreply.github.com>
2024-08-03 15:13:02 +0800

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
-# 交互式选择
+#!/bin/bash
+
 echo "请选择要执行的操作："
-echo "1. 开启BBR并安装必要依赖"
-echo "2. 安装Docker"
-echo "3. 安装宝塔面板"
-echo "4. 其他拥塞控制算法"
-echo "5. 哪吒面板"
-echo "6. x-ui面板"
-echo "7. 流媒体检测"
-echo "8. Swap"
-echo "9. 内网穿透frp安装"
-echo "10. 关闭ipv6"
-echo "11. 三网测速"
-echo "12. 修改命令提示符"
-echo "13. 修改SSH配置并更改密码"
-echo "14. 安装并连接Cloudflare WARP到40000端口"
-echo "15. 安装traffmonetizer（需要先自行安装docker）"
-echo "16. 通过DNS-01 验证给域名申请ACME证书"
-echo "17. 查看域名证书到期时间并手动选择是否强制更新证书"
-echo "18. 更新本脚本"
-echo "19. 可视化更改时区"
-echo "0. 返回"
+
+printf "%-3s %-3s %-3s %-3s\n" "1. 开启BBR并安装必要依赖" "2. 安装Docker" "3. 安装宝塔面板" "4. 其他拥塞控制算法"
+printf "%-3s %-3s %-3s %-3s\n" "5. 哪吒面板" "6. x-ui面板" "7. 流媒体检测" "8. Swap"
+printf "%-3s %-3s %-3s %-3s\n" "9. 内网穿透frp安装" "10. 关闭ipv6" "11. 三网测速" "12. 修改命令提示符"
+printf "%-3s %-3s %-3s %-3s\n" "13. 修改SSH配置并更改密码" "14. 安装并连接Cloudflare WARP到40000端口" "15. 安装traffmonetizer（需要先自行安装docker）" "16. 通过DNS-01 验证给域名申请ACME证书"
+printf "%-3s %-3s %-3s %-3s\n" "17. 查看域名证书到期时间并手动选择是否强制更新证书" "18. 更新本脚本" "19. 可视化更改时区" "0. 返回"
+
 
 read -p "请输入选项编号：" choice
 
@@ -245,31 +233,33 @@ case $choice in
     echo "选择操作"
     echo "1.表示查看证书到期时间"
     echo "2.表示强制更新所有证书"
-
+    echo
     read -p "请输入您的选择 (1 或 2): " choice
-
+     
     case $choice in
         1)
             echo "下载并运行查看证书到期时间的脚本..."
             curl -L https://raw.githubusercontent.com/EnochKingsley83/Rule/main/showdate.sh -o showdate.sh
             chmod +x showdate.sh
             ./showdate.sh
+            echo
             ;;
         2)
             echo "下载并运行强制更新证书的脚本..."
             curl -L https://raw.githubusercontent.com/EnochKingsley83/Rule/main/updatecert.sh -o updatecert.sh
             chmod +x updatecert.sh
             ./updatecert.sh
+            echo
             ;;
         *)
             echo "无效的选择。请输入 1 或 2。"
             ;;
     esac
+    echo
     ./"$0"
     ;;
     18)
-        curl -L https://raw.githubusercontent.com/EnochKingsley83/Rule/main/scripts.sh -o scripts.sh && chmod +x scripts.sh && sudo ./scripts.sh
-        exit
+        curl -L https://raw.githubusercontent.com/EnochKingsley83/Rule/main/scripts.sh -o scripts.sh && chmod +x scripts.sh
         ;;
     19)
         sudo dpkg-reconfigure tzdata
@@ -285,5 +275,6 @@ case $choice in
 esac
 
 echo "脚本执行完毕，重新运行中..."
+exit
 sleep 2
 ./"$0"
