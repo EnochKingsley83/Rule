3a7c5961f101103539e92e608f71a03e94a2a1fc
EnochKingsley83 <141494962+EnochKingsley83@users.noreply.github.com>
2024-08-03 14:01:04 +0800

    Create updatecert.sh

---
+++ updatecert.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 遍历所有域名文件夹并进行证书更新
+for domainDir in /root/DNScertificate/*; do
+    if [ -d "$domainDir" ]; then
+        domain=$(basename "$domainDir")
+        echo "正在检查和更新域名：$domain"
+
+        # 使用 acme.sh 检查证书是否需要更新并强制更新
+        ~/.acme.sh/acme.sh --renew -d "$domain" -d "*.$domain" --force
+
+        if [ $? -ne 0 ]; then
+            echo "证书更新失败，请检查错误日志。"
+            exit 1
+        else
+            echo "证书更新成功。"
+            echo "证书保存路径: $domainDir"
+        fi
+    fi
+done
+
+# 更新 acme.sh 脚本
+echo "正在更新acme.sh脚本..."
+~/.acme.sh/acme.sh --upgrade --auto-upgrade
+if [ $? -ne 0 ]; then
+    echo "acme.sh脚本自动更新失败，请检查错误日志。"
+else
+    echo "acme.sh脚本已成功更新。"
+fi
+
+echo "脚本执行完毕。"
