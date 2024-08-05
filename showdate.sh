992ea0b682adddeaf09aed041104f94c9f42f48b
EnochKingsley83 <141494962+EnochKingsley83@users.noreply.github.com>
2024-08-05 19:07:43 +0800

    Update showdate.sh

--- showdate.sh
+++ showdate.sh
@@ -4,9 +4,11 @@
 certPath="/root/cert"
 cd "$certPath" || { echo "无法访问目录 $certPath"; exit 1; }
 
-# 打印表头和分隔线
-printf "%-30s | %-30s | %-30s\n" "域名" "申请时间" "到期时间"
-printf "%s\n" "--------------------------------------------------------------"
+# 临时文件
+tempFile=$(mktemp)
+
+# 打印表头
+echo -e "域名\t申请时间\t到期时间" > "$tempFile"
 
 # 遍历每个子目录
 for dir in */; do
@@ -20,9 +22,18 @@ for dir in */; do
         issuedDate=$(openssl x509 -in "$certFile" -noout -startdate | sed 's/notBefore=//')
         expiryDate=$(openssl x509 -in "$certFile" -noout -enddate | sed 's/notAfter=//')
         
-        # 打印结果
-        printf "%-30s | %-30s | %-30s\n" "$domain" "$issuedDate" "$expiryDate"
+        # 打印结果到临时文件
+        echo -e "$domain\t$issuedDate\t$expiryDate" >> "$tempFile"
     else
-        printf "%-30s | %-30s | %-30s\n" "$domain" "证书文件缺失" "证书文件缺失"
+        # 证书文件缺失，打印相应的消息到临时文件
+        echo -e "$domain\t证书文件缺失\t证书文件缺失" >> "$tempFile"
     fi
 done
+
+# 打印分隔线和格式化输出
+echo "---------------------------------------------------------------"
+column -t -s $'\t' "$tempFile"
+echo "---------------------------------------------------------------"
+
+# 删除临时文件
+rm "$tempFile"
