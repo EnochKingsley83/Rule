ff54374ca697bc5b48aeeb9202bdbacb74c79e8b
EnochKingsley83 <141494962+EnochKingsley83@users.noreply.github.com>
2024-09-04 00:19:44 +0800

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -102,7 +102,7 @@ EOF
         # 安装aapanel
         URL=https://www.aapanel.com/script/install_7.0_en.sh && if [ -f /usr/bin/curl ];then curl -ksSO "$URL" ;else wget --no-check-certificate -O install_7.0_en.sh "$URL";fi;bash install_7.0_en.sh aapanel
         systemctl stop ufw
-        apt-get remove --purge ufw
+        apt-get remove --purge ufw -y
         rm -rf /etc/ufw
         ;;
     4)
